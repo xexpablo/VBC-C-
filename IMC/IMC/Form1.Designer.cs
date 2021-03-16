3665f160c185028c745a7b602481f9033c4a9c78
xexpablo <gameprores@gmail.com>
2021-03-15 23:17:14 -0300

    IMC

---
+++ IMC/IMC/Form1.Designer.cs
@@ -0,0 +1,223 @@
+﻿
+namespace IMC
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtPeso = new System.Windows.Forms.TextBox();
+            this.lblPeso = new System.Windows.Forms.Label();
+            this.lblAltura = new System.Windows.Forms.Label();
+            this.txtAltura = new System.Windows.Forms.TextBox();
+            this.indice = new System.Windows.Forms.GroupBox();
+            this.lblPesoResult = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblIdeal = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.lblMax = new System.Windows.Forms.Label();
+            this.lblMin = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.indice.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // txtPeso
+            // 
+            this.txtPeso.Location = new System.Drawing.Point(81, 37);
+            this.txtPeso.Name = "txtPeso";
+            this.txtPeso.Size = new System.Drawing.Size(100, 20);
+            this.txtPeso.TabIndex = 0;
+            // 
+            // lblPeso
+            // 
+            this.lblPeso.AutoSize = true;
+            this.lblPeso.Location = new System.Drawing.Point(28, 40);
+            this.lblPeso.Name = "lblPeso";
+            this.lblPeso.Size = new System.Drawing.Size(31, 13);
+            this.lblPeso.TabIndex = 1;
+            this.lblPeso.Text = "Peso";
+            // 
+            // lblAltura
+            // 
+            this.lblAltura.AutoSize = true;
+            this.lblAltura.Location = new System.Drawing.Point(28, 101);
+            this.lblAltura.Name = "lblAltura";
+            this.lblAltura.Size = new System.Drawing.Size(34, 13);
+            this.lblAltura.TabIndex = 2;
+            this.lblAltura.Text = "Altura";
+            // 
+            // txtAltura
+            // 
+            this.txtAltura.Location = new System.Drawing.Point(81, 98);
+            this.txtAltura.Name = "txtAltura";
+            this.txtAltura.Size = new System.Drawing.Size(100, 20);
+            this.txtAltura.TabIndex = 3;
+            // 
+            // indice
+            // 
+            this.indice.Controls.Add(this.lblPesoResult);
+            this.indice.Controls.Add(this.label2);
+            this.indice.Controls.Add(this.lblIdeal);
+            this.indice.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.indice.Location = new System.Drawing.Point(312, 12);
+            this.indice.Name = "indice";
+            this.indice.Size = new System.Drawing.Size(318, 80);
+            this.indice.TabIndex = 4;
+            this.indice.TabStop = false;
+            this.indice.Text = "Índice de Massa Corporal";
+            // 
+            // lblPesoResult
+            // 
+            this.lblPesoResult.AutoSize = true;
+            this.lblPesoResult.Font = new System.Drawing.Font("Calibri", 20F);
+            this.lblPesoResult.Location = new System.Drawing.Point(26, 37);
+            this.lblPesoResult.Name = "lblPesoResult";
+            this.lblPesoResult.Size = new System.Drawing.Size(64, 33);
+            this.lblPesoResult.TabIndex = 2;
+            this.lblPesoResult.Text = "0,00";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(28, 48);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 19);
+            this.label2.TabIndex = 1;
+            // 
+            // lblIdeal
+            // 
+            this.lblIdeal.AutoSize = true;
+            this.lblIdeal.Location = new System.Drawing.Point(108, 48);
+            this.lblIdeal.Name = "lblIdeal";
+            this.lblIdeal.Size = new System.Drawing.Size(72, 19);
+            this.lblIdeal.TabIndex = 0;
+            this.lblIdeal.Text = "Seu IMC";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.lblMax);
+            this.groupBox1.Controls.Add(this.lblMin);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(312, 115);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(318, 144);
+            this.groupBox1.TabIndex = 5;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Peso Ideal para sua Altura";
+            // 
+            // lblMax
+            // 
+            this.lblMax.AutoSize = true;
+            this.lblMax.Location = new System.Drawing.Point(126, 92);
+            this.lblMax.Name = "lblMax";
+            this.lblMax.Size = new System.Drawing.Size(121, 19);
+            this.lblMax.TabIndex = 3;
+            this.lblMax.Text = "Não Calculado";
+            // 
+            // lblMin
+            // 
+            this.lblMin.AutoSize = true;
+            this.lblMin.Location = new System.Drawing.Point(126, 41);
+            this.lblMin.Name = "lblMin";
+            this.lblMin.Size = new System.Drawing.Size(121, 19);
+            this.lblMin.TabIndex = 2;
+            this.lblMin.Text = "Não Calculado";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(28, 92);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(68, 19);
+            this.label5.TabIndex = 1;
+            this.label5.Text = "Máximo";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(28, 41);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(64, 19);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Mínimo";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(196, 69);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Calcular";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(695, 344);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.indice);
+            this.Controls.Add(this.txtAltura);
+            this.Controls.Add(this.lblAltura);
+            this.Controls.Add(this.lblPeso);
+            this.Controls.Add(this.txtPeso);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.indice.ResumeLayout(false);
+            this.indice.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtPeso;
+        private System.Windows.Forms.Label lblPeso;
+        private System.Windows.Forms.Label lblAltura;
+        private System.Windows.Forms.TextBox txtAltura;
+        private System.Windows.Forms.GroupBox indice;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label lblPesoResult;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblIdeal;
+        private System.Windows.Forms.Label lblMax;
+        private System.Windows.Forms.Label lblMin;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ IMC/IMC/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace IMC
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double peso = 0;
+            double altura = 0;
+            try
+            {
+                peso = Convert.ToDouble(txtPeso.Text);
+                altura = Convert.ToDouble(txtAltura.Text);
+                double imc = peso / (altura * altura);
+                if (imc < 17)
+                {
+                    lblIdeal.Text = "Muito abaixo do Peso";
+                }
+                else if(imc >= 17 && imc < 18.50)
+                {
+                    lblIdeal.Text = "Abaixo do Peso";
+                }
+                else if(imc >= 18.50 && imc < 25)
+                {
+                    lblIdeal.Text = "Peso Ideal";
+                }
+                else if(imc >= 25 && imc < 30)
+                {
+                    lblIdeal.Text = "Acima do Peso";
+                }
+                else if(imc >= 30 && imc < 35)
+                {
+                    lblIdeal.Text = "Obesidade I";
+                }
+                else if(imc >= 35 && imc < 40)
+                {
+                    lblIdeal.Text = "Obesidade II (Sevéra)";
+                }
+                else if(imc >= 40)
+                {
+                    lblIdeal.Text = "Obesidade III (Mórbida)";
+                }
+                lblPesoResult.Text = String.Format("{0:0.00}", imc);
+                lblMin.Text = (18.5 * (altura * altura)).ToString("F2") + " Kg";
+                lblMax.Text = (25 * (altura * altura)).ToString("F2") + " Kg";
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Preencha todos os Campos");
+            }
+        }
+    }
+}
